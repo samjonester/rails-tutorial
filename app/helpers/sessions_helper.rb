221dc25bf8feb4290e7c6688f5425ae0dcaf446b
Sam Jones <sam@samljones.com>
2015-11-29 21:07:40 -0500

    users list

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,7 +47,7 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
-
+  
   # Redirects to stored location (or to the default).
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url] || default)
